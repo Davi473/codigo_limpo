af68be6fa2d0a980895add6e30cf8abbf54727e9
Davi473 <68740118+Davi473@users.noreply.github.com>
2023-12-20 23:25:05 +0000

    confimar

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-app.use("/", express.static("/Site"));
+app.use("/", express.static("/Site/index.html"));
 
 // valores dos clientes sem banco de dados
 const clientesAdicionados = [
